d4abdd054c6e939564ec6e8a307b9a8f45cbdafb
Sergei <serfe121y@rambler.ru>
2023-03-17 19:21:35 +0300

    29

--- Task29DZ/Program.cs
+++ Task29DZ/Program.cs
@@ -3,3 +3,27 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
+int[] array = new int[8];
+
+void FillArray(int[] arr)
+{
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(0, 8);
+
+}
+}
+
+void PrintArray(int[] arr)
+{
+int count = arr.Length;
+for (int i = 0; i < count; i++)
+{
+Console.Write($"{arr[i] } ");
+}
+
+}
+
+FillArray(array);
+PrintArray(array);
