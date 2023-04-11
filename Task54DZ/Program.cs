77a36921051e43c3f01c3e511412e416d67c5844
Sergei <serfe121y@rambler.ru>
2023-04-11 19:22:46 +0300

    добавил 54дз, 56дз,62(списана)

---
+++ Task54DZ/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+
+
+//           0 1 2 3 
+//       0   1 4 7 2
+//       1   5 9 2 3
+//       2   8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void Streamline(int[,] array2d)
+{
+
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            for (int k = 0; k < array2d.GetLength(1) - 1; k++)
+            {
+                if (array2d[i, k] < array2d[i, k + 1])
+                {
+                    int temp = array2d[i, k + 1];
+                    array2d[i, k + 1] = array2d[i, k];
+                    array2d[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void WriteArray(int[,] array2d)
+{
+  for (int i = 0; i < array2d.GetLength(0); i++)
+  {
+    for (int j = 0; j < array2d.GetLength(1); j++)
+    {
+      Console.Write(array2d[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+Console.WriteLine($"\nОтсортированный массив: ");
+Streamline(array2d);
+WriteArray(array2d);
+
+
---
+++ Task56DZ/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
+WriteArray(array2d);
+
+int[,] CreateMatrixRndInt(int rows, int columns , int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; 
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+ int SumLineElements(int[,] array2d,int i)
+{
+  int sumLine = array2d[i,0];
+  for (int j = 1; j < array2d.GetLength(1); j++)
+  {
+    sumLine += array2d[i,j];
+  }
+  return sumLine;
+}
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array2d, 0);
+for (int i = 1; i < array2d.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array2d, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+void WriteArray (int[,] array2d)
+{
+  for (int i = 0; i < array2d.GetLength(0); i++)
+  {
+    for (int j = 0; j < array2d.GetLength(1); j++)
+    {
+      Console.Write(array2d[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+
+
+// int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
+// SumLineElements(matrix);
+// WriteArray(array2d);
